a07896840c7fa877889a941380b1e10de91e1971
Dorian Weber <dorianweb57@gmail.com>
2026-03-27 13:40:06 +0100

    feat(Evenement): ajout du model pour évènement avec Validator

---
+++ src/main/java/com/mns/cda/filsrouge/model/Evenement.java
@@ -0,0 +1,30 @@
+package com.mns.cda.filsrouge.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Evenement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer idTypeEvenement;
+
+    @NotBlank
+    protected String nomTypeEvenement;
+
+    @NotBlank
+    protected String descriptionEvenement;
+
+    @NotBlank
+    protected String dateEvenement;
+}
