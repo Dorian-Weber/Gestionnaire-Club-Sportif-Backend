39729a8e81455b09f7cd19286e275751ceec1c32
Dorian Weber <dorianweb57@gmail.com>
2026-03-27 09:51:51 +0100

    feat(disciplineController): ajout de la doc pour l'API

--- src/main/java/com/mns/cda/filsrouge/controller/DisciplineController.java
+++ src/main/java/com/mns/cda/filsrouge/controller/DisciplineController.java
@@ -2,6 +2,10 @@ package com.mns.cda.filsrouge.controller;
 
 import com.mns.cda.filsrouge.dao.DisciplineDAO;
 import com.mns.cda.filsrouge.model.Discipline;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Tag(name = "Discipline", description = "API de gestion des différentes discipline")
 public class DisciplineController {
 
     private DisciplineDAO disciplineDAO;
@@ -21,11 +26,22 @@ public class DisciplineController {
     }
 
     @GetMapping("/discipline/liste")
+    @Operation(summary = "Récupère la liste de toute les disciplines",
+            description = "Cette méthode permet de récupérer la liste de toutes les disciplines présentes dans la base de données.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Liste des disciplines récupérée avec succès")
+    })
     public List<Discipline> getDisciplineList() {
         return disciplineDAO.findAll();
     }
 
     @GetMapping("/discipline/{id}")
+    @Operation(summary = "Récupérer une discipline par son ID",
+            description = "Cette méthode permet de récupérer les informations d'une discipline spécifique en utilisant son ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Discipline récupéré avec succès"),
+            @ApiResponse(responseCode = "404", description = "Discipline non trouvé")
+    })
     public ResponseEntity<Discipline> getDisciplineById(@PathVariable Integer id) {
 
         Optional<Discipline> optionalDiscipline = disciplineDAO.findById(id);
@@ -37,6 +53,11 @@ public class DisciplineController {
     }
 
     @PostMapping("/discipline")
+    @Operation(summary = "Ajoute une discipline à la base de données",
+            description = "Cette méthode permet de d'ajouter une nouvelle discipline en base de données.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Discipline ajoutée avec succès")
+    })
     public ResponseEntity<Discipline> create(@RequestBody Discipline disciplineToInsert) {
 
         disciplineToInsert.setIdDiscipline(null);
@@ -46,6 +67,12 @@ public class DisciplineController {
     }
 
     @DeleteMapping("/discipline/{id}")
+    @Operation(summary = "Supprime une discipline par son ID",
+            description = "Cette méthode permet de supprimer une discipline spécifique en utilisant son ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Discipline supprimée avec succès"),
+            @ApiResponse(responseCode = "404", description = "Discipline non trouvé")
+    })
     public ResponseEntity<Discipline> delete(@PathVariable Integer id) {
 
         Optional<Discipline> optionalDiscipline = disciplineDAO.findById(id);
@@ -59,6 +86,12 @@ public class DisciplineController {
     }
 
     @PutMapping("/discipline/{id}")
+    @Operation(summary = "Modifie une discipline par son ID",
+            description = "Cette méthode permet de modifier les informations d'une discipline spécifique en utilisant son ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Discipline modifiée avec succès"),
+            @ApiResponse(responseCode = "404", description = "Discipline non trouvé")
+    })
     public ResponseEntity<Discipline> update(
             @PathVariable Integer id,
             @RequestBody Discipline disciplineToUpdate) {
