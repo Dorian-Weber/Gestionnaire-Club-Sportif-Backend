0cbcdd21e89fbcc191e89e71de346cad07382517
Dorian Weber <dorianweb57@gmail.com>
2026-03-27 12:10:34 +0100

    feat(TypeEvenementController): ajout de la factorisation des routes, JsonDoc)

--- src/main/java/com/mns/cda/filsrouge/controller/TypeEvenementController.java
+++ src/main/java/com/mns/cda/filsrouge/controller/TypeEvenementController.java
@@ -2,6 +2,11 @@ package com.mns.cda.filsrouge.controller;
 
 import com.mns.cda.filsrouge.dao.TypeEvenementDAO;
 import com.mns.cda.filsrouge.model.TypeEvenement;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +16,31 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/type-evenement")
+@Tag(name = "Type évènement", description = "API de gestion des différents types d'évènement")
 public class TypeEvenementController {
 
-    @Autowired
-    protected TypeEvenementDAO typeEvenementDAO;
 
-    @GetMapping("/type-evenement/liste")
+    protected final TypeEvenementDAO typeEvenementDAO;
+
+    @GetMapping("/liste")
+    @Operation(summary = "Récupère la liste des différents types d'évènements",
+            description = "Cette méthode permet de récupérer la liste de tous les types d'évènements présents dans la base de données.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Liste des types d'évènements récupérée avec succès")
+    })
     public List<TypeEvenement> getTypeEvenementAll() {
         return typeEvenementDAO.findAll();
     }
 
-    @GetMapping("/type-evenement/{id}")
+    @GetMapping("/{id}")
+    @Operation(summary = "Récupérer un type d'évènement par son ID",
+            description = "Cette méthode permet de récupérer les informations d'un type d'évènement spécifique en utilisant son ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Type d'évènement récupéré avec succès"),
+            @ApiResponse(responseCode = "404", description = "Type d'évènement non trouvé")
+    })
     public ResponseEntity<TypeEvenement> getTypeEvenementById(@PathVariable int id) {
 
         Optional<TypeEvenement> optionalTypeEvenement = typeEvenementDAO.findById(id);
@@ -32,7 +51,12 @@ public class TypeEvenementController {
         return new ResponseEntity<>(optionalTypeEvenement.get(), HttpStatus.OK);
     }
 
-    @PostMapping("/type-evenement")
+    @PostMapping
+    @Operation(summary = "Ajoute un type d'évènement à la base de données",
+            description = "Cette méthode permet de d'ajouter un nouveau type d'évènement en base de données.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Type d'évènement ajoutée avec succès")
+    })
     public ResponseEntity<TypeEvenement> create(@RequestBody TypeEvenement typeEvenementToInsert) {
 
         typeEvenementToInsert.setIdTypeEvenement(null);
@@ -42,7 +66,13 @@ public class TypeEvenementController {
 
     }
 
-    @DeleteMapping("/type-evenement/{id}")
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Supprime un type d'évènement par son ID",
+            description = "Cette méthode permet de supprimer un type d'évènement spécifique en utilisant son ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Type d'évènement supprimée avec succès"),
+            @ApiResponse(responseCode = "404", description = "Type d'évènement non trouvé")
+    })
     public ResponseEntity<TypeEvenement> delete(@PathVariable int id) {
 
         Optional<TypeEvenement> optionalTypeEvenement = typeEvenementDAO.findById(id);
@@ -55,7 +85,13 @@ public class TypeEvenementController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("/type-evenement/{id}")
+    @PutMapping("/{id}")
+    @Operation(summary = "Modifie un type d'évènement par son ID",
+            description = "Cette méthode permet de modifier les informations d'un type d'évènement spécifique en utilisant son ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Type d'évènement modifiée avec succès"),
+            @ApiResponse(responseCode = "404", description = "Type d'évènement non trouvé")
+    })
     public ResponseEntity<Void> update(
             @PathVariable int id,
             @RequestBody TypeEvenement typeEvenementToUpdate) {
