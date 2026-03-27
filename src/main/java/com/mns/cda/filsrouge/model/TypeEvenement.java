cfa6b2f90bdac060c4697c9fb41ac989d90e9aa8
Dorian Weber <dorianweb57@gmail.com>
2026-03-27 11:50:37 +0100

    feat(TypeEvenement) : ajout des validator pour le model

--- src/main/java/com/mns/cda/filsrouge/model/TypeEvenement.java
+++ src/main/java/com/mns/cda/filsrouge/model/TypeEvenement.java
@@ -1,10 +1,8 @@
 package com.mns.cda.filsrouge.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,5 +19,7 @@ public class TypeEvenement {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer idTypeEvenement;
 
+    @NotBlank
+    @Column(unique = true)
     protected String nomTypeEvenement;
 }
