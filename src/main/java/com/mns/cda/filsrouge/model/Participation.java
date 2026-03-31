17dad212028c410765ae0cd116aeda8d4e5c9529
Dorian Weber <dorianweb57@gmail.com>
2026-03-31 22:02:47 +0200

    feat(Participation) : création de la table associatve ManytoMany dans la relation athlete et event

---
+++ src/main/java/com/mns/cda/filsrouge/model/Participation.java
@@ -0,0 +1,40 @@
+package com.mns.cda.filsrouge.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Participation {
+
+    @Embeddable
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Key implements Serializable {
+        @Column(name = "athlete_id")
+        Integer Athleteid;
+        @Column(name = "event_id")
+        Integer Eventid;
+    }
+
+    @EmbeddedId
+    private Key key;
+
+    @ManyToOne
+    @MapsId("Athleteid")
+    @JoinColumn(name = "athlete_id")
+    protected Athlete athlete;
+
+    @ManyToOne
+    @MapsId("Eventid")
+    @JoinColumn(name = "event_id")
+    protected Event event;
+}
