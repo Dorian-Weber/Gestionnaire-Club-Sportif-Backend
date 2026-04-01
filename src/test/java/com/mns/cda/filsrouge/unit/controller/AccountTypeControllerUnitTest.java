321905c44c174a7627e8623b2b79a7bb9f0d0cca
Dorian Weber <dorianweb57@gmail.com>
2026-04-01 16:43:11 +0200

    test(AccountType): ajout des test pour le model et controller AccountType

---
+++ src/test/java/com/mns/cda/filsrouge/unit/controller/AccountTypeControllerUnitTest.java
@@ -0,0 +1,103 @@
+package com.mns.cda.filsrouge.unit.controller;
+
+import com.mns.cda.filsrouge.controller.AccountTypeController;
+import com.mns.cda.filsrouge.mock.MockAccountTypeDao;
+import com.mns.cda.filsrouge.model.Event;
+import com.mns.cda.filsrouge.model.AccountType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class AccountTypeControllerUnitTest {
+
+    //Test de GetAll
+    @Test
+    public void getAccountTypeAll_MustReturnList() {
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+
+        List<AccountType> response = accountTypeController.getAccountTypeList();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertFalse(response.isEmpty());
+        Assertions.assertEquals(1, response.size());
+    }
+
+
+    // Test de GetByID
+    @Test
+    public void getAccountTypeByIdExist_MustReturnCode200() {
+
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+        ResponseEntity<AccountType> response = accountTypeController.getAccountTypeById(1);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void getAccountTypeByIdNotExist_MustReturnCode404() {
+
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+        ResponseEntity<AccountType> response = accountTypeController.getAccountTypeById(2);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    // Test de Create test qu'il y a bien creation et que l'id est bien mise a null
+    @Test
+    public void createAccountType_MustReturnCode201() {
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+        AccountType accountType = new AccountType(10,
+                "Test");
+
+        ResponseEntity<AccountType> response = accountTypeController.create(accountType);
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertNull(response.getBody().getIdAccountType());
+    }
+
+    // Test de Delete
+    @Test
+    public void deleteAccountTypeExist_MustReturnCode204() {
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+
+        ResponseEntity<AccountType> response = accountTypeController.delete(1);
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
+    }
+    @Test
+    public void deleteAccountTypeNotExist_MustReturnCode404() {
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+
+        ResponseEntity<AccountType> response = accountTypeController.delete(2);
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    // Test de Update
+
+    @Test
+    public void updateAccountType_MustReturnCode200() {
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+        AccountType accountType = new AccountType(10,
+                "Test");
+
+        ResponseEntity<AccountType> response = accountTypeController.update(1, accountType);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(1, response.getBody().getIdAccountType());
+    }
+
+    @Test
+    public void updateAccountTypeNotExist_MustReturnCode404() {
+        AccountTypeController accountTypeController = new AccountTypeController(new MockAccountTypeDao());
+        AccountType accountType = new AccountType(10, "Test");
+
+        ResponseEntity<AccountType> reponse = accountTypeController.update(2, accountType);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, reponse.getStatusCode());
+
+    }
+}
---
+++ src/test/java/com/mns/cda/filsrouge/unit/model/AccountTypeUnitTest.java
@@ -0,0 +1,35 @@
+package com.mns.cda.filsrouge.unit.model;
+
+import com.mns.cda.filsrouge.TestUtilitaire;
+import com.mns.cda.filsrouge.model.AccountType;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AccountTypeUnitTest {
+
+    public static Validator validator ;
+
+    @BeforeAll
+    public static void init(){
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    // Test pour valider que le nom du type de compte ne peut pas être vide
+    @Test
+    public void validAccountTypeWithAccountTypeNameBlank_MustBeNotValidated(){
+
+        AccountType accountType = new AccountType();
+        accountType.setAccountTypeName("");
+
+        boolean constraintExist = TestUtilitaire.constraintViolationExist(
+                validator.validate(accountType),
+                "AccountTypeName",
+                "NotBlank");
+        Assertions.assertTrue(constraintExist);
+    }
+
+
+}
