e3b3d7300c9ac4e9620b37f35b5a31611c60cf4e
Dorian Weber <dorianweb57@gmail.com>
2026-04-01 10:46:46 +0200

    test: mise en place des tests pour controller de Team

---
+++ src/test/java/com/mns/cda/filsrouge/mock/MockTeamDao.java
@@ -0,0 +1,170 @@
+package com.mns.cda.filsrouge.mock;
+
+import com.mns.cda.filsrouge.dao.TeamDAO;
+import com.mns.cda.filsrouge.model.Team;
+import com.mns.cda.filsrouge.model.Sport;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class MockTeamDao implements TeamDAO {
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Team> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Team> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return List.of();
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<Team> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Integer> integers) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Team getOne(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Team getById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Team getReferenceById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public <S extends Team> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Team> List<S> findAll(Example<S> example) {
+        return List.of();
+    }
+
+    @Override
+    public <S extends Team> List<S> findAll(Example<S> example, Sort sort) {
+        return List.of();
+    }
+
+    @Override
+    public <S extends Team> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Team> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Team> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Team, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    @Override
+    public <S extends Team> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Team> List<S> saveAll(Iterable<S> entities) {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Team> findById(Integer idTeam) {
+        if(idTeam == 1) {
+            return Optional.of(new Team(1, "Test"));
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public List<Team> findAll() {
+
+        return List.of(new Team(1, "Test"));
+    }
+
+    @Override
+    public List<Team> findAllById(Iterable<Integer> integers) {
+        return List.of();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(Team entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Integer> integers) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Team> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<Team> findAll(Sort sort) {
+        return List.of();
+    }
+
+    @Override
+    public Page<Team> findAll(Pageable pageable) {
+        return null;
+    }
+}
---
+++ src/test/java/com/mns/cda/filsrouge/unit/controller/TeamControllerUnitTest.java
@@ -0,0 +1,101 @@
+package com.mns.cda.filsrouge.unit.controller;
+
+import com.mns.cda.filsrouge.controller.TeamController;
+import com.mns.cda.filsrouge.mock.MockTeamDao;
+import com.mns.cda.filsrouge.model.Team;
+import com.mns.cda.filsrouge.model.Sport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class TeamControllerUnitTest {
+
+    //Test de GetAll
+    @Test
+    public void getTeamListAll_MustReturnList() {
+        TeamController teamController = new TeamController(new MockTeamDao());
+
+        List<Team> response = teamController.getTeamList();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertFalse(response.isEmpty());
+        Assertions.assertEquals(1, response.size());
+    }
+
+
+    // Test de GetByID
+    @Test
+    public void getTeamByIdExist_MustReturnCode200() {
+
+        TeamController teamController = new TeamController(new MockTeamDao());
+        ResponseEntity<Team> response = teamController.getTeamById(1);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void getTeamByIdNotExist_MustReturnCode404() {
+
+        TeamController teamController = new TeamController(new MockTeamDao());
+        ResponseEntity<Team> response = teamController.getTeamById(2);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    // Test de Create test qu'il y a bien creation et que l'id est bien mise a null
+    @Test
+    public void createTeam_MustReturnCode201() {
+        TeamController teamController = new TeamController(new MockTeamDao());
+        Team team = new Team(10, "Test");
+
+        ResponseEntity<Team> response = teamController.create(team);
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertNull(response.getBody().getIdTeam());
+    }
+
+    // Test de Delete
+    @Test
+    public void deleteTeamExist_MustReturnCode204() {
+        TeamController teamController = new TeamController(new MockTeamDao());
+
+        ResponseEntity<Team> response = teamController.delete(1);
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
+    }
+    @Test
+    public void deleteTeamNotExist_MustReturnCode404() {
+        TeamController teamController = new TeamController(new MockTeamDao());
+
+        ResponseEntity<Team> response = teamController.delete(2);
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    // Test de Update
+
+    @Test
+    public void updateTeam_MustReturnCode200() {
+        TeamController teamController = new TeamController(new MockTeamDao());
+        Team team = new Team(10, "Test");
+
+        ResponseEntity<Team> response = teamController.update(1, team);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(1, response.getBody().getIdTeam());
+    }
+
+    @Test
+    public void updateTeamNotExist_MustReturnCode404() {
+        TeamController teamController = new TeamController(new MockTeamDao());
+        Team team = new Team(10, "Test");
+
+        ResponseEntity<Team> reponse = teamController.update(2, team);
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, reponse.getStatusCode());
+
+    }
+}
