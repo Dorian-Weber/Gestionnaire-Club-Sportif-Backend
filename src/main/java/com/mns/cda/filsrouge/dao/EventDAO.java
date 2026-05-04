b64526da5412c0db5f41a692933c087909d3f29c
Dorian Weber <dorianweb57@gmail.com>
2026-05-04 16:07:08 +0200

    fix : retrait d'une requetes

--- src/main/java/com/mns/cda/filsrouge/dao/EventDAO.java
+++ src/main/java/com/mns/cda/filsrouge/dao/EventDAO.java
@@ -17,77 +17,54 @@ import java.util.Optional;
 @Repository
 public interface EventDAO extends JpaRepository<Event, Integer> {
 
- @Query("SELECT new com.mns.cda.filsrouge.dto.EventLight(e.idEvent, e.eventName, e.eventDate, s.sportName) " +
-         "FROM Event e " +
-         "INNER JOIN e.sport s " +
-         "WHERE e.eventDate >= current timestamp " +
-         "ORDER BY e.eventDate ASC " +
-         "LIMIT 3")
+    @Query("SELECT new com.mns.cda.filsrouge.dto.EventLight(e.idEvent, e.eventName, e.eventDate, s.sportName) " +
+            "FROM Event e " +
+            "INNER JOIN e.sport s " +
+            "WHERE e.eventDate >= current timestamp " +
+            "ORDER BY e.eventDate ASC " +
+            "LIMIT 3")
     List<EventLight> findNextEventLight();
 
- @Query("SELECT new com.mns.cda.filsrouge.dto.EventMedium (e.idEvent, " +
-         "e.eventName, " +
-         "e.eventDescription, " +
-         "e.eventDate, " +
-         "et.eventTypeName, " +
-         "s.sportName,(select count(se) " +
-                        "FROM Seat se " +
-                        "Join se.reservations r2 " +
-                        "WHERE r2.event = e),(select count(se2)" +
-                                            " FROM Seat se2)) " +
-         "FROM Event e " +
-         "join e.eventType et " +
-         "JOIN e.sport s " +
-         "WHERE e.eventDate >= current timestamp " +
-         "order by e.eventDate ASC")
- List<EventMedium> findEventMedium();
-
- @Query("select new com.mns.cda.filsrouge.dto.EventMedium(e.idEvent, " +
-                  "e.eventName, " +
-                  "e.eventDescription, " +
-                  "e.eventDate, " +
-                  "et.eventTypeName, " +
-                  "s.sportName,(select count(se) " +
-                                 "FROM Seat se " +
-                                 "Join se.reservations r2 " +
-                                 "WHERE r2.event = e),(select count(se2) " +
-                                                     " FROM Seat se2))  " +
-                  "FROM Event e " +
-                  "join e.eventType et " +
-                  "JOIN e.sport s " +
-                  "WHERE (:sportName IS NULL or s.sportName = :sportName) " +
-                  "and (:eventTypeName IS NULL or et.eventTypeName = :eventTypeName)" +
-                  "AND (:search IS NULL OR :search = '' OR " +
-                    "LOWER(e.eventName) LIKE LOWER(CONCAT( '%', :search, '%')) OR " +
-                    "LOWER(e.eventDescription) LIKE LOWER(CONCAT('%', :search, '%'))) " +
-                    "AND (e.eventDate >= :dateMin) " +
-                  "order by e.eventDate ASC")
-    List<EventMedium> findEventMediumByFilter(
-         @Param("sportName") String sportName,
-         @Param("eventTypeName") String eventTypeName,
-         @Param("search") String search,
-         @Param("dateMin") LocalDateTime dateMin);
-
-    @Query("SELECT new com.mns.cda.filsrouge.dto.EventFull (e.idEvent, " +
+    @Query("SELECT new com.mns.cda.filsrouge.dto.EventMedium (e.idEvent, " +
             "e.eventName, " +
             "e.eventDescription, " +
             "e.eventDate, " +
             "et.eventTypeName, " +
-            "s.sportName, " +
-            "t.teamName, " +
-            "(SELECT ta.athleteName " +
-                "From" +
-                        "(select count(se) " +
-                        "FROM Seat se " +
-                        "Join se.reservations r2 " +
-                        "WHERE r2.event = e),(select count(se2)" +
-                                            " FROM Seat se2)) " +
+            "s.sportName,(select count(se) " +
+            "FROM Seat se " +
+            "Join se.reservations r2 " +
+            "WHERE r2.event = e),(select count(se2)" +
+            " FROM Seat se2)) " +
             "FROM Event e " +
             "join e.eventType et " +
             "JOIN e.sport s " +
-            "JOIN e.teams t " +
-            "Join t.athletes ta " +
             "WHERE e.eventDate >= current timestamp " +
             "order by e.eventDate ASC")
-    List<EventFull> findEventFull();
+    List<EventMedium> findEventMedium();
+
+    @Query("select new com.mns.cda.filsrouge.dto.EventMedium(e.idEvent, " +
+            "e.eventName, " +
+            "e.eventDescription, " +
+            "e.eventDate, " +
+            "et.eventTypeName, " +
+            "s.sportName,(select count(se) " +
+            "FROM Seat se " +
+            "Join se.reservations r2 " +
+            "WHERE r2.event = e),(select count(se2) " +
+            " FROM Seat se2))  " +
+            "FROM Event e " +
+            "join e.eventType et " +
+            "JOIN e.sport s " +
+            "WHERE (:sportName IS NULL or s.sportName = :sportName) " +
+            "and (:eventTypeName IS NULL or et.eventTypeName = :eventTypeName)" +
+            "AND (:search IS NULL OR :search = '' OR " +
+            "LOWER(e.eventName) LIKE LOWER(CONCAT( '%', :search, '%')) OR " +
+            "LOWER(e.eventDescription) LIKE LOWER(CONCAT('%', :search, '%'))) " +
+            "AND (e.eventDate >= :dateMin) " +
+            "order by e.eventDate ASC")
+    List<EventMedium> findEventMediumByFilter(
+            @Param("sportName") String sportName,
+            @Param("eventTypeName") String eventTypeName,
+            @Param("search") String search,
+            @Param("dateMin") LocalDateTime dateMin);
 }
